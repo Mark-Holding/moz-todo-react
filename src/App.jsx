2c0d0d29360f8fa644808aecf196585c1ec41299
Mark H <mark-holding@hotmail.co.uk>
2025-03-26 14:19:15 +0000

    add task completed toggle with state update

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,27 @@ function App(props) {
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false};
     setTasks([...tasks, newTask]);
-  }
+  };
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
   const taskList = tasks?.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} />
       ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";    
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -26,7 +42,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
